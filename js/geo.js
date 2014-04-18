e163ceadc4d4ea17612e20fa4f789b33d6eaaad6
Clifford Anderson <anderson.clifford@gmail.com>
2014-04-18 13:05:48 -0500

    Changed initial zoom

--- js/geo.js
+++ js/geo.js
@@ -1,7 +1,7 @@
 $(function () {
     // Instantiate MapBox map
     var map = L.mapbox.map('map', 'miskotte.hf27oe0n');
-    map.setView([10.1, 125.6], 5);
+    map.setView([10.1, 125.6], 7);
     // Gather GeoJSON points from CouchDB/Cloudant using JSONP
     $.getJSON("https://cartography.cloudant.com/maps/_design/coordinates/_view/getPoints?callback=?", function (result) {
         var points = result.rows;
