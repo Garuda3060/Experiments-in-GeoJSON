9859b9b4e6aa28d09b084eb1fcf4d95faec12a37
Clifford Anderson <anderson.clifford@gmail.com>
2014-04-18 13:03:14 -0500

    Code cleanup and organization

---
+++ js/geo.js
@@ -0,0 +1,15 @@
+$(function () {
+    // Instantiate MapBox map
+    var map = L.mapbox.map('map', 'miskotte.hf27oe0n');
+    map.setView([10.1, 125.6], 5);
+    // Gather GeoJSON points from CouchDB/Cloudant using JSONP
+    $.getJSON("https://cartography.cloudant.com/maps/_design/coordinates/_view/getPoints?callback=?", function (result) {
+        var points = result.rows;
+        var geoJSON =[];
+        for (var i in points) {
+            geoJSON[ "locations"] = geoJSON.push(points[i].value);
+        }
+        // Plot GeoJSON points
+        L.mapbox.featureLayer(geoJSON).addTo(map);
+    });
+});
